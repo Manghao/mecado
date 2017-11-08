a4f4064e45f7078973aca3c72ec58c27f128b77c
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2017-11-08 08:49:12 +0100

    Adding navbar

--- site/src/Controllers/AppController.php
+++ site/src/Controllers/AppController.php
@@ -10,7 +10,7 @@ class AppController extends BaseController
 
     public function index(RequestInterface $request, ResponseInterface $response, $args)
     {
-
+        $this->render($response, 'app/index');
     }
 
 }
