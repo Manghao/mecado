eb19af1536fccba410ed4049d058a5532a8b5219
Steven W <steven.wilmouth1@etu.univ-lorraine.fr>
2017-11-09 11:18:46 +0100

    Affichage liste créateur

--- site/src/Controllers/ListController.php
+++ site/src/Controllers/ListController.php
@@ -49,11 +49,11 @@ class ListController extends BaseController
                 $liste = new Liste();
                 $liste->name=$request->getParam('list_title');
                 $liste->descr=$request->getParam('description');
-                $liste->dateExp=$request->getParam('end_date');
-                $liste->other_dest=$request->getParam('other_dest') === 'on' ? 1 : 0;
+                $liste->date_exp=date('Y-m-d H:i:s', strtotime($request->getParam('end_date')));
+                $liste->other_dest=is_null($request->getParam('other_dest')) ? 0 : 1;
 
                 //TMP
-                $liste->id_creator=66;
+                $liste->id_creator=Session::get('user')->id;
                 $liste->save();
 
                 return $this->redirect($response, 'list.listitems', ['id' => $liste->id]);
@@ -65,192 +65,125 @@ class ListController extends BaseController
         }
     }
 
-  public function creationlistForm(RequestInterface $request, ResponseInterface $response, $args)
-  {
-    $this->render($response, 'list/listcreation');
-  }
-
-  public function creationlist(RequestInterface $request, ResponseInterface $response, $args)
-  {
-    if (false === $request->getAttribute('csrf_status')) {
-      $this->flash('error', 'Une erreur est survenue pendant l\'envoi du formulaire !');
-      return $this->redirect($response, 'list.creationlist.form', $request->getparams());
-    } else {
-
-      $errors = [];
-
-      if (!Validator::stringType()->notEmpty()->validate($request->getParam('list_title'))) {
-        $errors['list_title'] = "Veuillez saisir un titre valide.";
-      }
-
-      if (!Validator::stringType()->notEmpty()->validate($request->getParam('description'))) {
-        $errors['description'] = "Veuillez saisir une descritpion valide.";
-      }
-
-      if (!Validator::notEmpty()->validate($request->getParam('end_date'))) {
-        $errors['end_date'] = "Veuillez saisir une date valide.";
-      }
-      $dateListe = strtotime($request->getParam('end_date'));
-      $date = strtotime(date('Y-m-d'));
-
-      if ($date > $dateListe) {
-        $errors['end_date'] = "Veuillez saisir une date qui n'est pas encore passée.";
-      }
-
-      if (empty($errors)) {
-        $liste = new Liste();
-        $liste->name=$request->getParam('list_title');
-        $liste->descr=$request->getParam('description');
-        $liste->date_exp=date('Y-m-d H:i:s', strtotime($request->getParam('end_date')));
-        $liste->other_dest=is_null($request->getParam('other_dest')) ? 0 : 1;
-
-        //TMP
-        $liste->id_creator=Session::get('user')->id;
-        $liste->save();
-
-        return $this->redirect($response, 'list.listitems', ['id' => $liste->id]);
-
-      } else {
-          $this->flash('errors', $errors);
-          return $this->redirect($response, 'user.register.form', $args);
-      }
+    public function createproductForm(RequestInterface $request, ResponseInterface $response, $args)
+    {
+        $this->render($response, 'list/createproduct');
     }
-  }
 
-  public function createproductForm(RequestInterface $request, ResponseInterface $response, $args)
-  {
-    $this->render($response, 'list/createproduct');
-  }
-
-  public function createproduct(RequestInterface $request, ResponseInterface $response, $args)
-  {
-    if (false === $request->getAttribute('csrf_status')) {
-      $this->flash('error', 'Une erreur est survenue pendant l\'envoi du formulaire !');
-      return $this->redirect($response, 'list.createproduct.form', $request->getparams());
-    } else {
-
-      $errors = [];
-
-      if (!Validator::stringType()->notEmpty()->validate($request->getParam('name'))) {
-        $errors['name'] = "Veuillez saisir un nom valide.";
-      }
-
-      if (!Validator::stringType()->notEmpty()->validate($request->getParam('description'))) {
-        $errors['description'] = "Veuillez saisir une descritpion valide.";
-      }
-
-      if (!Validator::url()->validate($request->getParam('link'))) {
-        $errors['link'] = "Veuillez saisir un lien valide.";
-      }
-
-      if (!Validator::floatval()->notEmpty()->validate($request->getParam('price'))) {
-        $errors['price'] = "Veuillez saisir un lien valide.";
-      }
-      if (!Validator::notEmpty()->validate($request->getParam('pic'))) {
-        $errors['pic'] = "Veuillez ajouter un fichier valide.";
-      }
-
-
-      if (empty($errors)) {
-        $liste = new Liste();
-        $liste->name=$request->getParam('list_title');
-        $liste->descr=$request->getParam('description');
-        $liste->date_exp=date('Y-m-d H:i:s', strtotime($request->getParam('end_date')));
-        $liste->other_dest=is_null($request->getParam('other_dest')) ? 0 : 1;
-
-        //TMP
-        $liste->id_creator=Session::get('user')->id;
-        $liste->save();
-        return $this->redirect($response, 'list.listitems', ['id' => $liste->id]);
-      } else {
-          $this->flash('errors', $errors);
-          return $this->redirect($response, 'user.register.form', $args);
-      }
-    }
-  }
-
-    public function listitems(RequestInterface $request, ResponseInterface $response, $args) {
-        /*var_dump($args);
-        $liste = new Liste();
-        $liste->name = 'test';
-        $liste->id_creator = 1;
-        $liste->save();*/
-
-        // a la place de args mettre ['id' => ] dans la redirection
-
-        $list = Liste::where('id', '=', $args['id'])
-            ->first();
-
-        if (!empty($liste)) {
-            $products = Product::get();
-
-            if (!empty($products)) {
-                $this->render($response, 'list/listitems', [
-                    'list' => $list,
-                    'products' => $products], $args);
-            } else {
-                return $this->redirect($response, 'index', $args);
-            }
+    public function createproduct(RequestInterface $request, ResponseInterface $response, $args)
+    {
+        if (false === $request->getAttribute('csrf_status')) {
+            $this->flash('error', 'Une erreur est survenue pendant l\'envoi du formulaire !');
+            return $this->redirect($response, 'list.createproduct.form', $request->getparams());
         } else {
-            return $this->redirect($response, 'index', $args);
+
+            $errors = [];
+
+            if (!Validator::stringType()->notEmpty()->validate($request->getParam('name'))) {
+                $errors['name'] = "Veuillez saisir un nom valide.";
+            }
+
+            if (!Validator::stringType()->notEmpty()->validate($request->getParam('description'))) {
+                $errors['description'] = "Veuillez saisir une descritpion valide.";
+            }
+
+            if (!Validator::url()->validate($request->getParam('link'))) {
+                $errors['link'] = "Veuillez saisir un lien valide.";
+            }
+
+            if (!Validator::floatval()->notEmpty()->validate($request->getParam('price'))) {
+                $errors['price'] = "Veuillez saisir un lien valide.";
+            }
+            if (!Validator::notEmpty()->validate($request->getParam('pic'))) {
+                $errors['pic'] = "Veuillez ajouter un fichier valide.";
+            }
+
+
+            if (empty($errors)) {
+                $liste = new Liste();
+                $liste->name=$request->getParam('list_title');
+                $liste->descr=$request->getParam('description');
+                $liste->date_exp=date('Y-m-d H:i:s', strtotime($request->getParam('end_date')));
+                $liste->other_dest=is_null($request->getParam('other_dest')) ? 0 : 1;
+
+                //TMP
+                $liste->id_creator=Session::get('user')->id;
+                $liste->save();
+                return $this->redirect($response, 'list.listitems', ['id' => $liste->id]);
+            } else {
+                $this->flash('errors', $errors);
+                return $this->redirect($response, 'user.register.form', $args);
+            }
         }
     }
 
-  public function listitems(RequestInterface $request, ResponseInterface $response, $args) {
-      $list = Liste::where('id', '=', $args['id'])
-          ->first();
+    public function listitems(RequestInterface $request, ResponseInterface $response, $args) {
+        $list = Liste::where('id', '=', $args['id'])
+            ->first();
 
-      if (!is_null($list)) {
-          $products = Product::whereNotIn('id', function($query) {
-              $query->select('id_prod')
-                  ->from('list_products');
-          })->get();
+        if (!is_null($list)) {
+            $products = Product::whereNotIn('id', function($query) {
+                $query->select('id_prod')
+                    ->from('list_products');
+            })->get();
 
-          if (!is_null($products)) {
-             $this->render($response, 'list/listitems', [
-                 'list' => $list,
-                 'products' => $products]);
-          } else {
-              $this->flash('error', 'Les produits n\'existent pas !');
-              return $this->redirect($response, 'index');
-          }
-      } else {
-          $this->flash('error', 'La liste n\'existe pas !');
-          return $this->redirect($response, 'index');
-      }
-  }
+            if (!is_null($products)) {
+                $this->render($response, 'list/listitems', [
+                    'list' => $list,
+                    'products' => $products]);
+            } else {
+                $this->flash('error', 'Les produits n\'existent pas !');
+                return $this->redirect($response, 'index');
+            }
+        } else {
+            $this->flash('error', 'La liste n\'existe pas !');
+            return $this->redirect($response, 'index');
+        }
+    }
 
-  public function additem(RequestInterface $request, ResponseInterface $response, $args) {
-      $list = Liste::where('id', '=', $args['id'])
-          ->first();
+    public function additem(RequestInterface $request, ResponseInterface $response, $args) {
+        $list = Liste::where('id', '=', $args['id'])
+            ->first();
 
-      if (!is_null($list)) {
-          $product = Product::where('id', '=', $args['idProd'])
-              ->first();
+        if (!is_null($list)) {
+            $product = Product::where('id', '=', $args['idProd'])
+                ->first();
 
-          if (!is_null($product)) {
-              $list_product = new ListProducts();
-              $list_product->id_list = $list->id;
-              $list_product->id_prod = $product->id;
-              $list_product->save();
+            if (!is_null($product)) {
+                $list_product = new ListProducts();
+                $list_product->id_list = $list->id;
+                $list_product->id_prod = $product->id;
+                $list_product->save();
 
-              $this->flash('success', 'Le produit "' . $product->name . '" a bien été ajouté à votre liste !');
-              return $this->redirect($response, 'list.listitems', ['id' => $list->id]);
-          } else {
-              $this->flash('error', 'Le produit n\'existe pas !');
-              return $this->redirect($response, 'index');
-          }
-      } else {
-          $this->flash('error', 'La liste n\'existe pas !');
-          return $this->redirect($response, 'index');
-      }
-  }
+                $this->flash('success', 'Le produit "' . $product->name . '" a bien été ajouté à votre liste !');
+                return $this->redirect($response, 'list.listitems', ['id' => $list->id]);
+            } else {
+                $this->flash('error', 'Le produit n\'existe pas !');
+                return $this->redirect($response, 'index');
+            }
+        } else {
+            $this->flash('error', 'La liste n\'existe pas !');
+            return $this->redirect($response, 'index');
+        }
+    }
 
-  public function view(RequestInterface $request, ResponseInterface $response, $args)
-  {
+    public function view(RequestInterface $request, ResponseInterface $response, $args)
+    {
         $list = Liste::where('id', '=', $args['id'])->first();
         if (!is_null($list)) {
-            $this->render($response, 'list/view', ['list' => $list]);
+            $products = Product::join('list_products', 'list_products.id_prod', '=', 'product.id')
+                ->join('list', 'list.id', '=', 'list_products.id_list')
+                ->where('list.id', '=', $list->id)
+                ->select(
+                    'product.*',
+                    'list_products.reserve as isReserved',
+                    'list_products.user_reserve as userReserve'
+                )
+                ->get();
+            $this->render($response, 'list/view', [
+                'list' => $list,
+                'products' => $products
+            ]);
         } else {
             $this->flash('error', 'La liste demandée n\'existe pas ou est introuveable.');
             return $this->redirect($response, 'index');
@@ -264,9 +197,9 @@ class ListController extends BaseController
             $url = $_SERVER['REQUEST_SCHEME'] . '://';
             $url .= $_SERVER['HTTP_HOST'];
             $url .= $this->container->get('router')
-                        ->pathFor('list.view.shared', [
-                            'token' => uniqid()
-                        ]);
+                ->pathFor('list.view.shared', [
+                    'token' => uniqid()
+                ]);
 
             $list->url_share = $url;
             $list->save();
